32222762ad6cfbb92e91178987ebed9db9eb79e6
Ashima Arora <ashimaarora63@gmail.com>
2022-06-23 21:51:43 -0400

    modified weather api

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const https = require("https");
+
 const app = express();
 
 app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8e0ae2fdc2946a2cd3cc00484af48383";
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+    })
     res.send("Server is running")
 })
+
 
 
 app.listen(3000, () => {
