df2ba237e9c2808414cd20bca16370e4b959cf69
Ashima Arora <ashimaarora63@gmail.com>
2022-07-09 22:01:58 -0400

    newsletter-signup3

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static("public")); //to serve up static files such as styles.css or png file
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+})
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
