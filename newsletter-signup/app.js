4f49ae7366ebd5245f1bc3dd1ab32cde650c379b
Ashima Arora <ashimaarora63@gmail.com>
2022-07-10 22:04:22 -0400

    Newsletter signup4

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -7,7 +7,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    res.send("Thank you for signing up to our newsletter!")
+    console.log(req.body.firstName);
 })
+
+//5eb6de5bcb6086a446c3a37eb3bd66b3-us10
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
